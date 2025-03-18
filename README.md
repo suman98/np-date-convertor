b2084a76be03b1950334e9c5ea0da21566a8841a
suman98 <41943773+suman98@users.noreply.github.com>
2025-03-18 10:56:14 +0545

    Updated package

---
+++ README.md
@@ -0,0 +1,8 @@
+# np-date-convertor
+
+A simple utility to convert English dates to Nepali dates.
+
+## Installation
+
+```sh
+npm install np-date-convertor
---
+++ index.js
@@ -0,0 +1,6 @@
+function convertToNepaliDate(englishDate) {
+  // Replace this with your own date conversion logic
+  return `Converted Nepali Date for ${englishDate}`;
+}
+
+module.exports = { convertToNepaliDate };
